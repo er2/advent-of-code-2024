43ec0cc534519f24a2edb2359d4a705de21484bc
Eric Riese <4515131+er2@users.noreply.github.com>
2024-12-01 12:50:04 -0500

    day 1, both parts

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Advent of Code 2024](https://adventofcode.com/2024)
+
+In Clojure
+
+Name inputs `resources/dayX.txt`
---
+++ src/advent_of_code_2024/day1.clj
@@ -0,0 +1,59 @@
+(ns advent-of-code-2024.day1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn readlines []
+  (str/split-lines (slurp (io/resource "day1.txt"))))
+
+(defn line-to-pair [l]
+  (mapv parse-long (str/split l #"\s+")))
+
+(defn to-pairs [lines]
+  (map line-to-pair lines))
+
+(defn unzip [pairs]
+  (let [l (map first pairs)
+        r (map second pairs)]
+    [l r]))
+
+(defn sort-lists [pairs]
+  (map sort (unzip pairs)))
+
+(defn to-matched-pairs [lists]
+  (map vector (first lists) (second lists)))
+
+(defn distance [[l r]]
+  (abs (- l r)))
+
+(defn calc-total-distance []
+  (->> (readlines)
+      (to-pairs)
+      (sort-lists)
+      (to-matched-pairs)
+      (map distance)
+      (reduce +)))
+
+(defn proc-similarity [[ns occurences]]
+  (let [freqs (frequencies occurences)
+        calc (fn [n] (* n (freqs n 0)))]
+    (map calc ns)))
+
+(defn calc-similarity []
+  (->> (readlines)
+       (to-pairs)
+       (unzip)
+       (proc-similarity)
+       (reduce +)))
+
+(comment
+  (readlines)
+  (line-to-pair "3   4")
+  (to-pairs (readlines))
+  (sort-lists (to-pairs (readlines)))
+  (to-matched-pairs (sort-lists (to-pairs (readlines))))
+  (to-matched-pairs (to-pairs (readlines)))
+  (distance [1 3])
+  (calc-total-distance)
+  (frequencies [1 2 2 4])
+  (proc-similarity [[1] [1 1]])
+  (calc-similarity))
