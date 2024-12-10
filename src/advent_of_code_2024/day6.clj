14beac2ee00a5dad127464b895f74b2d3d6c6d36
Eric Riese <4515131+er2@users.noreply.github.com>
2024-12-10 00:22:18 -0500

    day 6 wip

--- src/advent_of_code_2024/day6.clj
+++ src/advent_of_code_2024/day6.clj
@@ -29,10 +29,10 @@
    :max-y     (count lines)})
 
 (def forward
-  {\^ (fn [[x y]] [x (dec y)])
-   \< (fn [[x y]] [(dec x) y])
-   \> (fn [[x y]] [(inc x) y])
-   \v (fn [[x y]] [x (inc y)])})
+  {\^ (fn up [[x y]] [x (dec y)])
+   \< (fn left [[x y]] [(dec x) y])
+   \> (fn right [[x y]] [(inc x) y])
+   \v (fn down [[x y]] [x (inc y)])})
 
 (def rotate
   {\^ \>
@@ -41,18 +41,22 @@
    \< \^})
 
 (defn in-map [map]
-  (fn [[x y]]
-    (and
-      (< -1 x (:max-x map))
-      (< -1 y (:max-y map)))))
+  (let [{max-x :max-x
+         max-y :max-y} map]
+    (fn [c]
+      (let [[x y] c]
+        (and
+          (< -1 x max-x)
+          (< -1 y max-y))))))
 
 (defn one-more
   "takes a function and a collection.
   returns a new collection based on the original
   plus a new element from the function applied to the previous last element"
-  [last-fn one]
-  (let [new-last-elem (last-fn (last one))]
-    (conj one new-last-elem)))
+  [last-fn coll]
+  (if (empty? coll) []
+                    (let [new-last-elem (last-fn (last coll))]
+                      (conj (vec coll) new-last-elem))))
 
 (defn why [last]
   (if (:unobstructed last)
@@ -60,10 +64,13 @@
     :obstructed))
 
 (defn explain-ending [moves]
-  (let [[last & others] (reverse moves)
-        reason (why last)]
+  (let [reason (why (last moves))]
     {:reason reason
-     :moves  (reverse others)}))
+     :moves  (reverse (drop-last 1 moves))}))
+
+(defn my-print [e]
+  (prn e)
+  e)
 
 (defn walk-forward [m]
   (let [{guard     :guard
@@ -75,13 +82,14 @@
         unobstructed (fn [coord] (not (obstacles coord)))
         detailed (fn [c] {:in-map       (in-map c)
                           :unobstructed (unobstructed c)
-                          :coord        c})]
+                          :coord        c})
+        next (fn [q] (let [c (:coord q)] (detailed (forward c))))]
     (->>
       (iterate forward loc)
       (map detailed)
       (take-while (every-pred :in-map :unobstructed))
-      (one-more #(detailed (forward (:coord %))))
-      ;(one-more (comp :coord forward detailed))
+      (vec)
+      (one-more next)
       (explain-ending)
       )))
 
@@ -91,7 +99,7 @@
         new-orientation (rotate (:dir (:guard m)))
         walked-off-edge (= reason :off-map)
         new-trod (conj (:trod map) moves)
-        new-pos (last moves)
+        new-pos (:coord (last moves))
         new-map (assoc m :trod new-trod :guard {:loc new-pos :dir new-orientation})]
     (if walked-off-edge
       new-map
@@ -104,6 +112,9 @@
       (:trod)
       (count)))
 
+(defn -main []
+  (prn (part1)))
+
 (comment
   (readlines)
   (range 10)
@@ -113,6 +124,7 @@
   (parse-obstacles (readlines))
   (parse-guard (readlines))
   (parse-map (readlines))
+  (patrol (parse-map (readlines)))
   ((in-map (parse-map (readlines))) [0 0])
   (walk-forward (parse-map (readlines)))
   ;(post-pend inc (lazy-seq ))
