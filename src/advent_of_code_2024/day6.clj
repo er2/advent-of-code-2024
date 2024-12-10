a1e6edb38db024bb57683caeef3820b4bc30bf3d
Eric Riese <4515131+er2@users.noreply.github.com>
2024-12-09 22:31:35 -0500

    day 6 wip

--- src/advent_of_code_2024/day6.clj
+++ src/advent_of_code_2024/day6.clj
@@ -24,7 +24,7 @@
 (defn parse-map [lines]
   {:obstacles (parse-obstacles lines)
    :guard     (parse-guard lines)
-   :walked    #{}
+   :trod      #{}
    :max-x     (.length (first lines))
    :max-y     (count lines)})
 
@@ -34,30 +34,88 @@
    \> (fn [[x y]] [(inc x) y])
    \v (fn [[x y]] [x (inc y)])})
 
+(def rotate
+  {\^ \>
+   \> \v
+   \v \<
+   \< \^})
+
 (defn in-map [map]
   (fn [[x y]]
     (and
       (< -1 x (:max-x map))
       (< -1 y (:max-y map)))))
 
-(defn walk-forward [map]
+(defn one-more
+  "takes a function and a collection.
+  returns a new collection based on the original
+  plus a new element from the function applied to the previous last element"
+  [last-fn one]
+  (let [new-last-elem (last-fn (last one))]
+    (conj one new-last-elem)))
+
+(defn why [last]
+  (if (:unobstructed last)
+    :off-map
+    :obstructed))
+
+(defn explain-ending [moves]
+  (let [[last & others] (reverse moves)
+        reason (why last)]
+    {:reason reason
+     :moves  (reverse others)}))
+
+(defn walk-forward [m]
   (let [{guard     :guard
-         obstacles :obstacles} map
+         obstacles :obstacles} m
         {loc :loc
          dir :dir} guard
         forward (forward dir)
-        in-map (in-map map)
-        unobstructed (fn [coord] (not (obstacles coord)))]
+        in-map (in-map m)
+        unobstructed (fn [coord] (not (obstacles coord)))
+        detailed (fn [c] {:in-map       (in-map c)
+                          :unobstructed (unobstructed c)
+                          :coord        c})]
     (->>
       (iterate forward loc)
-      (take-while (every-pred in-map unobstructed)))))
+      (map detailed)
+      (take-while (every-pred :in-map :unobstructed))
+      (one-more #(detailed (forward (:coord %))))
+      ;(one-more (comp :coord forward detailed))
+      (explain-ending)
+      )))
+
+(defn patrol [m]
+  (let [{reason :reason
+         moves  :moves} (walk-forward m)
+        new-orientation (rotate (:dir (:guard m)))
+        walked-off-edge (= reason :off-map)
+        new-trod (conj (:trod map) moves)
+        new-pos (last moves)
+        new-map (assoc m :trod new-trod :guard {:loc new-pos :dir new-orientation})]
+    (if walked-off-edge
+      new-map
+      (recur new-map))))
+
+(defn part1 []
+  (-> (readlines)
+      (parse-map)
+      (patrol)
+      (:trod)
+      (count)))
 
 (comment
   (readlines)
   (range 10)
+  (part1)
+  ;({:foo {:bar 4}} :foo)
   (take 5 (iterate (forward \>) [0 0]))
   (parse-obstacles (readlines))
   (parse-guard (readlines))
   (parse-map (readlines))
   ((in-map (parse-map (readlines))) [0 0])
-  (walk-forward (parse-map (readlines))))
+  (walk-forward (parse-map (readlines)))
+  ;(post-pend inc (lazy-seq ))
+  (explain-ending [{:in-map false :unobstructed true :coord [1 1]}])
+  (explain-ending [{} {} {:in-map true :unobstructed false :coord [1 1]}])
+  (take 5 (walk-forward (parse-map (readlines)))))
